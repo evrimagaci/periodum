22f7a83e70a25adf2794be195339edfe8d557bc1
kerem <kurtulus1934@hotmail.com>
2022-01-24 00:56:46 +0300

    content updated

--- components/DeviceOrientation.vue
+++ components/DeviceOrientation.vue
@@ -2,7 +2,6 @@
   <el-dialog
     :visible="visible"
     :show-close="false"
-    class="device-orientation"
     :before-close="beforeClose"
     :close-on-click-modal="false"
     :modal-append-to-body="false"
@@ -11,10 +10,14 @@
     style="padding: 10px"
   >
     <img src="../assets/icons/icons-close.svg" class="close-btn" @click="beforeClose" />
-    <div slot="title" style="text-align: center">
+    <div style="text-align: center">
       <img src="../assets/images/group-26@3x.png" style="width: 100px; height: 100px; margin: auto" />
     </div>
-    <div class="dialog-body"></div>
+    <div class="do-dialog-body">
+      <span class="do-dialog-title"
+        ><span>Yan Çevir</span> <span style="display: inline-block; width: 100%; font-size: 1.1rem; font-weight: 600; padding: 15px 0">veya</span> <span>PC'ye Geç</span></span
+      >
+    </div>
   </el-dialog>
 </template>
 
@@ -44,8 +47,23 @@ export default {
 @import '~@/assets/css/partials/variables';
 .close-btn {
   cursor: pointer;
-  top: -24px;
-  right: 0;
+  top: 24px;
+  right: 24px;
   position: absolute;
+}
+.do-dialog-body {
+  position: relative;
+  display: inline-block;
+  text-align: center;
+  margin: 20px;
+  padding-top: 25px;
+}
+.do-dialog-title {
+  font-size: 1.6rem;
+  color: $white;
+  font-weight: 900;
+  span {
+    white-space: nowrap;
+  }
 }
 </style>
